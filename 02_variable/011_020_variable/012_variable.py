8dc2c8eca4f4b214689c50f1af235f1d7ecc055e
syhwang1029 <syhwang@itall.com>
2024-11-28 11:08:23 +0900

    011~020 디렉토리 묶음

---
+++ 02_variable/011_020_variable/012_variable.py
@@ -0,0 +1,13 @@
+# 012  ariable
+street = 298000000000000 #시가총액
+present = 50000 # 현재가
+PER = 15.79 # PER
+print("street: "+str(street)) 
+print(present)
+print(PER)
+# 풀이 : street의 변수값이 int형이기 때문에, p
+# rint 안에 문자열(street:) 을 추가할 경우에는 sting 타입 변형(str(street)))이 필요함.
+# 결과물 :
+# street: 298000000000000
+# 50000
+# 15.79

8949dc3cc55c659404315749cdae26aab37b55a3
syhwang1029 <syhwang@itall.com>
2024-11-28 16:10:32 +0900

    108 문제풀이

---
+++ 07_if/101_110_if/108_If.py
@@ -0,0 +1,12 @@
+# 108 if
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there.")
+# 풀이 : 예상 결과는 "Hi, there." 임.
+# 3은 4보다 작기 때문에 False임.
+# if문은 조건에 할당된 경우(True),
+# else문은 조건이 할당되지 그외의 경우(False)에 실행됨.
+# 그리하여 조건(4 < 3)에 할당되지 않아, else문이 실행될 것임.
+
+# 결과 : Hi, there.

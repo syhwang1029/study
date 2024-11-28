6c616855c4b1fc85b62f44a452c8261b6a00c74e
syhwang1029 <syhwang@itall.com>
2024-11-28 15:24:12 +0900

    001~010 불필요 디렉토리 삭제 및 이동

--- 01_python_start/001_010_print/001_010_print/002_print.py
+++
@@ -1,4 +0,0 @@
-# 002 print
-print("Mary's cosmetics")
-# 풀이 : 같은 따옴표 끼리는 중복적으로 사용을 못함 -> " ' ' " (o), " "" " (x)
-# 결과물 : Hello World
--- 01_python_start/001_010_print/001_010_print/008_print.py
+++
@@ -1,4 +0,0 @@
-# 008 print
-print('naver/kakao/sk/samsung')
-# 풀이 : print는 공백없이 문자열을 붙여쓰면 그상태로 출력됨
-# 결과물 : naver/kakao/sk/samsung

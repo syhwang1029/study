67f6db24136e66a7479c74ee26acd4c7309e9004
syhwang1029 <syhwang@itall.com>
2024-11-28 13:14:20 +0900

    071 문제풀이

---
+++ 05_tuple/071_080_tuple/071_tuple.py
@@ -0,0 +1,13 @@
+# 5. 파이썬 튜플
+# 튜플은 순서가 있지만, 수정 불가능한 자료구조임
+
+# 071~080
+
+# 071 tuple
+my_variable = ()
+print(my_variable) 
+print(type(my_variable)) #타입확인용
+
+# 풀이 : 튜플의 기본 형식은 변수명 = () 이지만, 소괄호() 생략도 가능함
+# 결과 : ()
+# <class 'tuple'>

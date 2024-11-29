cfabdde91ce9149514c200e05c9fbba11b2e72af
syhwang1029 <syhwang@itall.com>
2024-11-29 16:37:15 +0900

    151 변수명 수정

--- 08_for/151_160_for/151_for.py
+++ 08_for/151_160_for/151_for.py
@@ -3,9 +3,9 @@
 # 151 for
 numls =  [3, -20, -3, 44] # 정수 리스트
 
-for nega in numls: # nega 변수에 리스트 할당
-    if nega < 0: # 0보다 작은 경우, 즉 음수인 경우가 조건임
-        print(nega) # 음수만 출력
+for negative in numls: # negative 변수에 리스트 할당
+    if negative< 0: # 0보다 작은 경우, 즉 음수인 경우가 조건임
+        print(negative) # 음수만 출력
         
 # 결과 :
 # -20

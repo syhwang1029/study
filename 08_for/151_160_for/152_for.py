f0bf7606e67fe031fab3ffd4a3d717094a94591e
syhwang1029 <syhwang@itall.com>
2024-11-29 16:58:41 +0900

    152 문제풀이

---
+++ 08_for/151_160_for/152_for.py
@@ -0,0 +1,13 @@
+# 152 for  
+
+# 숫자 리스트
+numls = [3, 100, 23, 44] 
+
+# drainage 변수에 리스트 할당
+for drainage in numls:
+    # 요소를 3과 나눔 
+    if drainage % 3 == 0:
+        # 3의 배수만 출력
+        print(drainage)
+
+# 결과 : 3

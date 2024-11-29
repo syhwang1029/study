aac7017411f4572789f3f2a321bef54d635cf16e
syhwang1029 <syhwang@itall.com>
2024-11-29 14:59:22 +0900

    135 문제풀이

---
+++ 08_for/131_140_for/135_for.py
@@ -0,0 +1,28 @@
+# 135 for
+for ABC in ["A", "B", "C"]:
+  b = ABC.lower()
+  print("변환:", b)
+# 변환: a
+# 변환: b
+# 변환: c
+
+# 위와 같은 결과 함수 작성
+# 대문자A를 변수값으로 할당 
+a1 = "A"
+# 소문자로 변환
+a1 = a1.lower()
+
+b2 = "B"
+b2 = b2.lower()
+c3 = "C"
+c3 = c3.lower()
+
+# 소문자a 출력
+print("변환: "+a1)
+
+print("변환: "+b2)
+print("변환: "+c3)
+# 결과 :
+# 변환: a
+# 변환: b
+# 변환: c

fbca708541b256a6ef2ea725e61db464c73a132a
syhwang1029 <syhwang@itall.com>
2024-11-29 14:59:06 +0900

    133 문제풀이

---
+++ 08_for/131_140_for/133_for.py
@@ -0,0 +1,16 @@
+# 133 for
+for ABC in ["A", "B", "C"]:
+  print(ABC) 
+# A 
+# B 
+# C
+
+# 위와 같은 결과 함수 작성
+# \n으로 줄바꿈
+abc = ("\na \nb \nc")
+# 대문자로 변환
+print(abc.upper())
+# 결과 : 
+# A 
+# B 
+# C

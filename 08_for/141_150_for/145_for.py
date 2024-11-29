dee94587382de91809bc924042b0847742fadaa6
syhwang1029 <syhwang@itall.com>
2024-11-29 16:12:34 +0900

    145 문제풀이

---
+++ 08_for/141_150_for/145_for.py
@@ -0,0 +1,10 @@
+# 145 for
+
+animal = ['dog', 'cat', 'parrot'] # 동물 리스트
+for name in animal: # name 변수에 리스트 요소 할당
+    print(name[0]) # for문에서는 리스트 한요소당 한라인씩 출력, 
+                    # 0번째 인덱스인 맨앞글자만 출력
+# 결과 : 
+# d
+# c
+# p

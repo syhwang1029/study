c06ecbddb2acbfb6db861b19a66eedff4a17b590
syhwang1029 <syhwang@itall.com>
2024-11-29 15:27:22 +0900

    123 해결 시도

--- 07_if/121_130_if/123_if.py
+++ 07_if/121_130_if/123_if.py
@@ -1,22 +1,21 @@
 # 123 if * 답안 확인
  
 # 각종 나라별 돈 딕셔너리
-money = {
-        '달러':1167, 
-        '엔':1.096,
-        '유료':1268,
-        '위안':171
-         }
+money = {"달러":1167, "엔":1.096, "유료":1268, "위안":171}
+
 # value 값만 생성
-money_values = money.values()
+# money_values = money.values()
+# money_values = float(money_values)
 
 # 입력받은 데이터
 users = input("입력: ")
+num, money  = users.split()
+
 # 숫자형 변환
-user = float(users)
+#user = float(users)
 
 # 입력받은 데이터 * 돈  
-won = user * money_values
+#won = money_values * user 
 
 #결과
-print(won)
+print(float(num) * money, "원")

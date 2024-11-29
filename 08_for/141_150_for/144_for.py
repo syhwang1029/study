7ea13b901b616f546aefca12937f8f61939f7d55
syhwang1029 <syhwang@itall.com>
2024-11-29 16:12:30 +0900

    144 문제풀이

---
+++ 08_for/141_150_for/144_for.py
@@ -0,0 +1,10 @@
+# 144 for
+
+animal = ['dog', 'cat', 'parrot'] # 동물 리스트 
+for lens in animal: # lens 변수에 리스트 요소 할당
+    print(lens, len(animal)) # 리스트 데이터, 각 문자길이 출력
+
+# 결과 : 
+# dog 3
+# cat 3
+# parrot 3

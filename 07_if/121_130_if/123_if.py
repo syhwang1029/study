b1407d2608242faed53aa70f7d837e0781ea325a
syhwang1029 <syhwang@itall.com>
2024-12-02 08:54:50 +0900

    123 문제해결

--- 07_if/121_130_if/123_if.py
+++ 07_if/121_130_if/123_if.py
@@ -1,21 +1,24 @@
-# 123 if * 답안 확인
- 
-# 각종 나라별 돈 딕셔너리
-money = {"달러":1167, "엔":1.096, "유료":1268, "위안":171}
+# 123 if *답안 확인
 
-# value 값만 생성
-# money_values = money.values()
-# money_values = float(money_values)
+# 나라별 돈 딕셔너리 
+# 단위:금액
+money = {'달러':1167, 
+         '엔':1.069, 
+         '유료':1268, 
+         '위안':171}
 
-# 입력받은 데이터
+# 입력받은 값
+# str
 users = input("입력: ")
-num, money  = users.split()
 
-# 숫자형 변환
-#user = float(users)
+# 공백() 기준으로 분할 후 두 변수에 값 할당
+num, currency = users.split()
 
-# 입력받은 데이터 * 돈  
-#won = money_values * user 
+# 환전 = 입력 값 * 환율 = 원
+# num : float로 형변환 
+# currency : 입력 값(단위)은 money에서 key로 value값 호출(float)
+print(float(num) * money[currency], "원")
 
-#결과
-print(float(num) * money, "원")
+# 결과 : 
+# 입력: 1000 달러
+# 1167000.0 원

141bd9438a54f7da6b02c6bdb6adc7b33fafc092
syhwang1029 <syhwang@itall.com>
2024-12-02 14:18:30 +0900

    256 풀이 추가

--- 11_class/251_260_class/256_class.py
+++ 11_class/251_260_class/256_class.py
@@ -2,6 +2,7 @@
 
 # Human 클래스 정의
 class Human:
+    # 매개변수
     # self 로 생성자 정의
     # 이름, 나이, 성별 
     def __init__(self, name, age, gender):
@@ -12,7 +13,7 @@ class Human:
 # 인스턴스 생성자
 areum = Human("조아름", 25, "여자")
 # 나이, 이름, 성별 생성자 출력
-# 인스턴스 속성 접근
+# 인스턴스 접근
 print("이름: ", areum.name, ", 나이: ", areum.age, ", 성별: ", areum.gender)
 
 # 결과 :

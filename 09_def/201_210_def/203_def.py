df25f9977c2645993875bfe2ff1f0a419734d39b
syhwang1029 <syhwang@itall.com>
2024-12-02 09:04:56 +0900

    203 문제풀이

---
+++ 09_def/201_210_def/203_def.py
@@ -0,0 +1,19 @@
+# 203 def
+
+# 203 def 
+
+# def 키워드로 print_coin() 함수 정의
+def  print_coin():
+    # 함수에 문자 할당
+    print("비트 코인")
+
+# 함수 호출 * 100
+for a in range(100):
+    print_coin()
+
+# 결과 :
+# 비트 코인
+# 비트 코인
+# 비트 코인
+# ...
+# 비트 코인

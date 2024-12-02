44a5a67c13a3c93867158bb3c03bd70462a7d820
syhwang1029 <syhwang@itall.com>
2024-12-02 09:29:26 +0900

    260 문제풀이

---
+++ 11_class/251_260_class/260_class.py
@@ -0,0 +1,18 @@
+# 260 class 
+
+# class OMG : 
+#     def print() : # 빈 생성자
+#         print("Oh my god") 
+
+# myStock = OMG() # 빈 인스턴스 
+# myStock.print() # 빈 속성
+# TypeError Traceback (most recent call last)
+# <ipython-input-233-c85c04535b22> in <module>()
+# myStock.print()
+
+# TypeError: print() takes 0 positional arguments but 1 was given
+# 결과 :
+#     TypeError Traceback (most recent call last)
+#               ^^^^^^^^^
+# SyntaxError: invalid syntax
+

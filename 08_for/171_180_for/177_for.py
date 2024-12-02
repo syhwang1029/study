03de37a910e7f2ed5ac562faccbc5e63e702d3e9
syhwang1029 <syhwang@itall.com>
2024-12-02 09:00:30 +0900

    177 문제풀이

---
+++ 08_for/171_180_for/177_for.py
@@ -0,0 +1,16 @@
+# 177 for *답안 확인
+
+# 리스트
+my_list = ["가", "나", "다", "라"]
+# 1번씩 반복
+for a in range(1):
+    # 리스트 요소 중 인덱스 번호 
+    # 0번째 "가", 1번째 "나" 
+    print(my_list[3], my_list[2])
+    print(my_list[2], my_list[1])
+    print(my_list[1], my_list[0])
+
+# 결과 :
+# 라 다
+# 다 나
+# 나 가

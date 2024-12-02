94c1dc3b7c98b14da189ee530dc04a6b6144aecc
syhwang1029 <syhwang@itall.com>
2024-12-02 09:25:29 +0900

    232 문제풀이

---
+++ 09_def/231_240_def/232_def.py
@@ -0,0 +1,14 @@
+# 232 def
+
+# 함수 정의
+# 매개변수 정의
+def make_url(str232):
+    # 반환 값에 문자열 함께 나열
+    print("www."+str232+".com")
+
+# 파라미터 값 출력
+make_url("naver")
+# www.naver.com
+
+# 결과 :
+# www.naver.com

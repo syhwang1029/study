69e174034e5c121f40e57fdf6c6798f85c73a69f
syhwang1029 <syhwang@itall.com>
2024-12-02 09:22:46 +0900

    215 문제풀이

---
+++ 09_def/211_220_def/215_def.py
@@ -0,0 +1,17 @@
+# 215 def
+
+# 함수 정의
+def print_with_smile(str):
+    # 매개변수로 문자 전달
+    print(str)
+
+# 입력받은 데이터
+users = input("입력: ")
+
+# 함수 호출
+# 입력 데이터 + 문자 출력
+print_with_smile(users+":D")
+
+# 결과 : 
+# 입력: 안녕하세요
+# 안녕하세요:D

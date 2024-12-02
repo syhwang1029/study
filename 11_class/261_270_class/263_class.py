232bfa13251d3e3369cdba2cb583cc623246b181
syhwang1029 <syhwang@itall.com>
2024-12-02 09:29:47 +0900

    263 문제풀이

---
+++ 11_class/261_270_class/263_class.py
@@ -0,0 +1,23 @@
+# 263 class 메서드 1 *답안 확인
+
+# 클래스 정의
+class Stock:
+    # 생성자 정의
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+    # 메서드 추가
+    def set_name(self, name):
+        self.name = name 
+
+# 객체 정의
+# 초기화 
+a = Stock(None, None)
+
+# 메서드 호출
+a.set_name("삼성전자")
+# 속성 출력
+print(a.name)
+
+# 결과 : 삼성전자

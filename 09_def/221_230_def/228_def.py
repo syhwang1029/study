819d391ab1cdac93b39fe612897b4b626c6a1ca3
syhwang1029 <syhwang@itall.com>
2024-12-02 09:24:20 +0900

    228 문제풀이

---
+++ 09_def/221_230_def/228_def.py
@@ -0,0 +1,12 @@
+# 228 def
+
+# 함수 정의
+# 매개변수 정의
+def calc_monthly_salary(annual_salary) :
+    # 숫자 형변환
+    # 파라미터 값 / 12개월 = 월급  
+    print(int(annual_salary/12))
+calc_monthly_salary(12000000)
+
+# 결과 :
+# 1000000

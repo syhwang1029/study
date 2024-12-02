fbda18515d2072b8130f0c68783192d0adb8eb3f
syhwang1029 <syhwang@itall.com>
2024-12-02 09:23:30 +0900

    222 문제풀이

---
+++ 09_def/221_230_def/222_def.py
@@ -0,0 +1,12 @@
+# 221 del *답안 확인
+
+# 함수 정의
+# 매개변수 정의
+def print_score(score_list):
+    # 리스트 요수 총합/리스트 요소갯수
+    print(sum(score_list)/len(score_list))
+
+# 평균 결과값 출력
+print_score ([1, 2, 3])
+
+# 결과 : 2.0

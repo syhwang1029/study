8e58b00e81d2095114e13e2f2052a8ac0ad084b0
syhwang1029 <syhwang@itall.com>
2024-12-02 09:28:55 +0900

    254 문제풀이

---
+++ 11_class/251_260_class/254_class.py
@@ -0,0 +1,14 @@
+# 254 class 클래스 생성자 1
+
+# Human 클래스 정의
+class Human:
+    # 생성자 정의
+    def __init__(self):
+        # 출력  
+        print("응애응애")
+
+# areum 객체 생성 
+areum = Human()
+
+# 결과 :
+# 응애응애

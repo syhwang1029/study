a367751e95921fbdb7324481a3e9a5ebec436c16
syhwang1029 <syhwang@itall.com>
2024-12-02 09:05:08 +0900

    206 문제풀이

---
+++ 09_def/201_210_def/206_def.py
@@ -0,0 +1,30 @@
+# 206 def 
+
+# 함수 정의
+def message() :
+    # 실행 문장
+    print("A")
+    print("B")
+
+# 첫번째 호출
+message() # A,B
+
+print("C") # C
+
+# 두번째 호출
+message() # A,B
+
+# 예상 결과 
+# A
+# B 
+# C
+# A
+# B
+
+
+# 결과 : 
+# A
+# B
+# C
+# A
+# B

cc4c1afb432cac46b2b2c1d7b2ca0e7a6488729a
syhwang1029 <syhwang@itall.com>
2024-12-02 09:04:59 +0900

    204 문제풀이

---
+++ 09_def/201_210_def/204_def.py
@@ -0,0 +1,18 @@
+# 204 def
+
+# print_coins() 함수 정의 
+def print_coins():
+    # 100번 호출
+    for conins in range(100):
+        # 함수에 문자 할당
+        print("비트코인")
+
+# 함수 호출
+print_coins()   
+
+# 결과 : 
+# 비트 코인
+# 비트 코인
+# 비트 코인
+# ...
+# 비트 코인

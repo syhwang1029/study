10777e943bc83e2a7f1beb65194aa6a250cb6d58
syhwang1029 <syhwang@itall.com>
2024-12-02 09:25:57 +0900

    238 문제풀이

---
+++ 09_def/231_240_def/238_def.py
@@ -0,0 +1,14 @@
+# 238 def
+
+def fun1(num) :
+    return num + 4
+
+def fun2(num) :
+    return num * 10
+
+a = fun1(10) # 10+4=14
+c = fun2(a) # 14*10=140
+print(c) # 140
+# 예상 결과 : 140
+
+# 결과 : 140

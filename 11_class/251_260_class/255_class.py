eb86fd0f98ee110d99077cd36a18d1523fd2ba62
syhwang1029 <syhwang@itall.com>
2024-12-02 14:18:17 +0900

    255 풀이 추가

--- 11_class/251_260_class/255_class.py
+++ 11_class/251_260_class/255_class.py
@@ -2,6 +2,7 @@
 
 # Human 클래스 정의
 class Human:
+    # 매개변수 정의
     # self로 생성자 정의
     # 이름, 나이, 성별
     def __init__(self, name, age, gender):
@@ -10,8 +11,9 @@ class Human:
         self.gender = gender 
         
 # areum 인스턴스 생성
-# 객체의 속성 정의  
+# 객체의 속성값 정의
 areum = Human("아름", 25, "여자")
+# 매개변수 호출
 print(areum.name, areum.age, areum.gender)
 
 # 결과 : 아름 25 여자

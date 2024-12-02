f2c04fcbaf357e13a59da5dee64fb824aa125e85
syhwang1029 <syhwang@itall.com>
2024-12-02 14:19:01 +0900

    266 풀이 추가

--- 11_class/261_270_class/266_class.py
+++ 11_class/261_270_class/266_class.py
@@ -3,14 +3,17 @@
 
 # 클래스 정의 
 class Stock:
+    # 메서드 정의
     # 생성자 정의
-    # 종목명, 종목코드, PER, PBR, 배당수익률 생성자
+    # 종목명, 종목코드 생성자
+    # PER, PBR, 배당수익률 생성자 추가
     def __init__(self, name, code, per, pbr, money):
         self.name = name
         self.code = code 
-        self.per = per
-        self.pbr = pbr 
-        self.money = money
+        # 속성 없데이트
+        self.per = per #PER 생성자 추가
+        self.pbr = pbr # PBR 생성자 추가
+        self.money = money # 배당수익률 생성자 추가
     
     # 종목명 메서드 추가
     def set_name(self, name):
@@ -20,8 +23,7 @@ class Stock:
     def set_code(self, code):
         self.code = code
     
-    
-    # 종목명 속성 업데이트 
+    # 종목명 속성 업데이트   
     def get_name(self):
         return self.name
     

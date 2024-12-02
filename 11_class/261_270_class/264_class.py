c5ef4aee2ed5a3340e6dbcf8221f0bd86bc680da
syhwang1029 <syhwang@itall.com>
2024-12-02 09:29:51 +0900

    264 문제풀이

---
+++ 11_class/261_270_class/264_class.py
@@ -0,0 +1,23 @@
+# 264 class 메서드 2
+
+# 클래스 정의 
+class Stock:
+    # 생성자 정의 
+    def __init__(self, name, code):
+        self.name = name 
+        self.code = code
+
+    # 메서드 추가
+    def set_code(self, code):
+        self.code = code
+
+# 객체 정의
+# None로 초기화
+a = Stock(None, None)
+
+# 메서드 호출 
+a.set_code("005930")
+# 속성 출력
+print(a.code)     
+
+# 결과 : 005930

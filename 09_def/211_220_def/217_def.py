8228942fcf6b2db5612f8f0b9e833fd59a01e8f8
syhwang1029 <syhwang@itall.com>
2024-12-02 09:22:54 +0900

    217 문제풀이

---
+++ 09_def/211_220_def/217_def.py
@@ -0,0 +1,17 @@
+# 217 def
+
+# 함수 정의
+def print_upper_price(int217):
+    print(int217)
+
+# 입력 받은 데이터     
+users = input("가격 입력: ")
+# 30% 상한가
+# 실수형 변환(입력 데이터) * 1.3 = 30% 상한가
+print_upper_price(float(users) * 1.3)
+
+# 결과 : 
+# 가격 입력: 10000
+# 13000.0
+
+

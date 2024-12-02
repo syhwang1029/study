8678468b45ef9d00ae364d443318a38b54958a8c
syhwang1029 <syhwang@itall.com>
2024-12-02 09:24:37 +0900

    231 문제풀이

---
+++ 09_def/231_240_def/231_def.py
@@ -0,0 +1,16 @@
+# 231~240
+
+# 231 def
+# 함수 정의
+def n_plus_1 (n) :
+    result = n + 1
+    # 함수 밖에 다가 변수 정의
+n_plus_1(3)
+# print(result) 
+# 에러 발생
+
+
+# 결과 :
+#     print(result)
+#           ^^^^^^
+# NameError: name 'result' is not defined

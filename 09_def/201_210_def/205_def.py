c7355052e4760ee06773ea31e2ca23103f18f7ef
syhwang1029 <syhwang@itall.com>
2024-12-02 09:05:03 +0900

    205 문제풀이

---
+++ 09_def/201_210_def/205_def.py
@@ -0,0 +1,22 @@
+# 205 def
+
+# 함수 호출
+# hello()
+## 함수 정의 전에 호출하여 에러발생
+
+# 함수 정의 
+def hello():
+    # 함수 문자 할당
+    print("Hi")
+
+# 결과 :
+#     hello()
+#     ^^^^^
+# NameError: name 'hello' is not defined. Did you mean: 'help'?
+
+# 올바른 함수 호출법
+# hello() 
+  
+
+
+    
